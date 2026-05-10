2eeea49175a087e08fca8943354aa40ae4dc86b4
Somesh Patil <67376135+funkymoose@users.noreply.github.com>
2026-05-10 06:38:28 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,61 @@
+from fastapi import FastAPI
+import requests
+
+app = FastAPI()
+
+cache = {}
+
+@app.get("/book/{isbn}")
+def get_book(isbn: str, API_KEY: str):
+
+    # CACHE CHECK
+    if isbn in cache:
+        return {
+            "source": "cache",
+            "data": cache[isbn]
+        }
+
+    # url = f"https://www.googleapis.com/books/v1/volumes?q=isbn:{isbn}"
+    # API_KEY = "AIzaSyDJRAGgnzYI2NZTwVV6_ttuuirReke90yo"
+
+    url = f"https://www.googleapis.com/books/v1/volumes?q=isbn:{isbn}&key={API_KEY}"
+
+    response = requests.get(url, timeout=20)
+
+    if response.status_code != 200:
+        return {
+            "success": False,
+            "status": response.status_code,
+            "error": response.text
+        }
+
+    data = response.json()
+
+    if "items" not in data:
+        return {
+            "success": False,
+            "message": "Book not found"
+        }
+
+    volume = data["items"][0]["volumeInfo"]
+
+    result = {
+        "title": volume.get("title"),
+        "subtitle": volume.get("subtitle"),
+        "authors": volume.get("authors", []),
+        "categories": volume.get("categories",[]),
+        "publisher": volume.get("publisher"),
+        "publishedDate": volume.get("publishedDate"),
+        "description": volume.get("description"),
+        "pageCount":volume.get("pageCount"),
+        "thumbnail": volume.get("imageLinks", {}).get("thumbnail"),
+        "rating": volume.get("averageRating")
+    }
+
+    # SAVE TO CACHE
+    cache[isbn] = result
+
+    return {
+        "source": "google",
+        "data": result
+    }
